cf5841fc88514b8888f03b4ee7c86086c9e2f765
Holger Dehnhardt <holger@dehnhardt.org>
2016-07-06 22:47:51 +0200

    disable filter by group as the setting of the group is broken

--- dashboard/services/widgetmanagementservice.php
+++ dashboard/services/widgetmanagementservice.php
@@ -54,13 +54,14 @@ class WidgetManagementService {
 
         foreach ($allWidgets as $widget) {
             $enabledGroups = explode(':', $widget['enabled_groups']);
-            if($filterByUser) {
+            //TODO eventually reenable filter by group
+            /*if($filterByUser) {
                 if($this->isWidgetForUserAllowed($this->user, $enabledGroups)) {
                     $return[] = $widget['wId'];
                 }
-            } else {
+            } else {*/
                 $return[] = $widget['wId'];
-            }
+            //}
         }
         return $return;
     }
